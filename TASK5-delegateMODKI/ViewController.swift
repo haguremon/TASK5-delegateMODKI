698ab42b158d40e1bf041798a912aa12bfb1e086
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-27 07:32:24 +0900

    Initial Commit

---
+++ TASK5-delegateMODKI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TASK5-delegateMODKI
+//
+//  Created by IwasakIYuta on 2021/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
