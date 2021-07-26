2a2f1f07eeb75a05501f2da61deed1005c81e1de
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-27 07:40:27 +0900

    デリゲートについて理解するのは難しいけど使いこなせたら最高だろうに

---
+++ TASK5-delegateMODKI/ArithmeticOperator.swift
@@ -0,0 +1,72 @@
+//
+//  ArithmeticOperator.swift
+//  TASK5-delegateMODKI
+//
+//  Created by IwasakIYuta on 2021/07/27.
+//
+
+import Foundation
+import UIKit
+//委譲するプロトコルで宣言
+//必要な条件
+protocol ArithmeticOperatorDelegate: AnyObject{
+    func number1() -> Any //もっといい取得の方法があるはず・・・
+    func number2() -> Any
+}
+
+//処理を委任するクラス
+class ArithmeticOperator {
+    weak var delegate: ArithmeticOperatorDelegate?
+    weak var viewController: ViewController?
+    func dialogAlert(message: String) {
+        let alert = UIAlertController(title: "Task5", message: message, preferredStyle: .alert )
+        alert.addAction(UIAlertAction(title: "ok", style: .default))
+        self.viewController?.present(alert, animated: true)
+    }
+    //割り算をする処理
+    func dividedBy(resultLabel: UILabel){
+        guard let divisor = delegate?.number1() as? Double else {
+           //Any型をダウンキャストできなかった場合ここがよばれる
+            return dialogAlert(message: "割られる数を入力してください")
+        
+        }
+        guard let  dividend = delegate?.number2() as? Double else {
+            
+            return dialogAlert(message: "割る数を入力してください")
+        
+        }
+        if dividend.isZero {
+          return dialogAlert(message: "割る時は０を入力しないでください")
+       
+        } else {
+        
+        resultLabel.text = String(divisor / dividend)
+        
+        }
+    }
+    //ここから下はいらないけど作った色々とカスタマイズできるの最高
+    func Addition(resultLabel: UILabel){
+        guard let number1 = delegate?.number1() as? Double,
+              let  number2 = delegate?.number2() as? Double else { return }
+        
+        resultLabel.text = String(number1 + number2)
+    }
+    func Subtraction(resultLabel: UILabel){
+        guard let number1 = delegate?.number1() as? Double,
+              let  number2 = delegate?.number2() as? Double else { return }
+        
+        resultLabel.text = String(number1 - number2)
+    }
+    func Multiplication(resultLabel: UILabel){
+        guard let number1 = delegate?.number1() as? Double,
+              let  number2 = delegate?.number2() as? Double else { return }
+        
+        resultLabel.text = String(number1 * number2)
+    }
+    func remainder(resultLabel: UILabel){
+        guard let number1 = delegate?.number1() as? Int,
+              let  number2 = delegate?.number2() as? Int else { return }
+        
+        resultLabel.text = String(number1 % number2)
+    }
+}
--- TASK5-delegateMODKI/ViewController.swift
+++ TASK5-delegateMODKI/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private var number1TextField: UITextField!
+    @IBOutlet private var number2TextField: UITextField!
+    
+    @IBOutlet private var resultLabel: UILabel!
+    private  let arithmeticOperator = ArithmeticOperator()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        arithmeticOperator.delegate = self
+        arithmeticOperator.viewController = self
+        number1TextField.keyboardType = .decimalPad
+        number2TextField.keyboardType = .decimalPad
     }
-
-
+    
+    @IBAction func dividedByButton(_ sender: UIButton) {
+        arithmeticOperator.dividedBy(resultLabel: resultLabel)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}//ここでの記述量を減らしたい//Any型はあまり使わない方がいいが・・・
+    
+extension ViewController: ArithmeticOperatorDelegate {
+    func number1() -> Any {
+        guard let divisor = Int(number1TextField.text!) else {
+            return ""
+        }
+        return divisor
+    }
+    func number2() -> Any {
+        guard let dividend = Int(number2TextField.text!) else {
+            return ""
+        }
+        return dividend
+    }
 }
 
